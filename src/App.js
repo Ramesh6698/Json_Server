739f883a978fe2e529921d8fae518af0ebbcdb71
Ramesh6698 <rameshsekar6698@gmail.com>
2023-05-02 19:51:10 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,63 +1,64 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import React ,{useState}from 'react'
+import "./App.css"
 
-const App = () => {
-  const [inputval, setinputval] = useState("");
-  const [inputArr, setinputArr] = useState([]);
 
-  const addItems = () => {
-    if (!inputval) {
-      alert("Please give some Data");
-    } else {
-      setinputArr([...inputArr, inputval]);
-      setinputval("");
+
+const App=()=> {
+  const [result,setResult]=useState(" ");
+
+  const handleClick =(e) =>{
+    setResult(result.concat(e.target.name));
+
+    }
+    const Clear = () => {
+      setResult("");
+    }
+    const backspace = () => {
+      setResult(result.slice(0, result.length -1));
+    }
+
+    const Calculate = () => {
+      try {
+      setResult(eval(result).toString());
+    } catch(err) {
+      setResult("Error");
     }
   };
-  const DeleteItem = (id) => {
-    const newArr = inputArr.filter((val, ind) => {
-      return id !== ind;
-    });
-    setinputArr(newArr);
-  };
+
   return (
-    <div className="App">
-      <div>
-        <h2>Todo Task</h2> <br></br>
-        <input
-          type="text"
-          placeholder="Enter Your Task"
-          value={inputval}
-          onChange={(e) => {
-            console.log(e.target.value);
+    <>
+    <div className="container">
+      <form>
+        <input type="text" value={result}></input>
+      </form>
+      <div className="keypad" >
+        <button  className='heighlight' onClick={Clear} id="Clear">Clear</button>
+        <button  className='heighlight' onClick={backspace} id ="backspace">Delete</button>
+        <button  className='heighlight' name="."onClick={handleClick}>.</button>
 
-            setinputval(e.target.value);
-          }}
-        ></input>
-        <button className="btn btn-primary" onClick={addItems}>
-          {" "}
-          Add Task
-        </button>
-      </div>
+        <button  className='heighlight' name="/" onClick={handleClick}>/ </button>
 
-      <div className="flex-wrapper">
-        {inputArr.map((val, ind) => {
-          return (
-            <div className="card1" key={ind}>
-              <div className="text-wrapper">{val} </div>
-              <button
-                className="btn btn-danger"
-                onClick={() => DeleteItem(ind)}
-              >
-                {" "}
-                Delete
-              </button>
-            </div>
-          );
-        })}
+        <button  className='heighlight' name="7" onClick={handleClick}>7</button>
+        <button  className='heighlight' name="8" onClick={handleClick}>8</button>
+        <button  className='heighlight' name="9" onClick={handleClick}>9</button>
+        <button  className='heighlight' name="*"onClick={handleClick}>&times; </button>
+        <button  className='heighlight' name="4" onClick={handleClick}>4</button>
+        <button  className='heighlight' name="5"onClick={handleClick}>5</button>
+        <button  className='heighlight' name="6" onClick={handleClick}>6</button>
+        <button  className='heighlight' name="-"onClick={handleClick}>&ndash;</button>
+        <button  className='heighlight' name="1"onClick={handleClick}>1</button>
+        <button  className='heighlight' name="2" onClick={handleClick}>2</button>
+        <button  className='heighlight' name="3"onClick={handleClick}>3</button>
+        <button  className='heighlight' name="+"onClick={handleClick}>+</button>
+        <button  className='heighlight' name="0"onClick={handleClick} id ="zero">0</button>
+        <button  className='heighlight' onClick={Calculate} id ="result">=</button>
+
+
+
       </div>
     </div>
+    </>
   );
 };
 
-export default App;
+export default App
