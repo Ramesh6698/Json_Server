3dd2eca047678f293365d570a6de296ce950c3f1
Ramesh6698 <rameshsekar6698@gmail.com>
2023-05-06 17:27:34 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import Shop from './Component/Shop';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import React, { useState, useEffect } from "react";
+import Shop from "./Component/Shop";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 function App() {
+  const [data, setdata] = useState([]);
+
+  const logJSONData = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const jsonData = await response.json();
+    console.log("-jsonData", response, jsonData);
+    setdata(jsonData);
+  };
+
+  useEffect(() => {
+    logJSONData();
+  }, []);
+
   return (
     <div>
-    <h1> Online Shopping </h1>
+      <h1> Online Shopping </h1>
 
-    <Shop></Shop>
+      <Shop data={data} />
     </div>
   );
-};
+}
 
-export default App
+export default App;
--- src/Component/Props.js
+++ src/Component/Props.js
@@ -1,19 +1,23 @@
 import React from "react";
 
-class Prop extends React.Component{
-
-    render(){
-        return(
-            <div class="card" >
-  <img src={this.props.pic} class="card-img-top" alt="..."></img>
-  <div class="card-body" >
-    <h5 class="card-title">{this.props.title}</h5>
-    <p class="card-text"> {this.props.text} </p>
-    <a href={this.props.link} class="btn btn-primary"> Buy Now</a>
-  </div>
-</div>
-        )
-    };
-};
+class Prop extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <img src={this.props.pic} class="card-img-top" alt="..."></img>
+        <div className="card-body">
+          <h5 className="card-title">{this.props.title}</h5>
+          <p className="card-text"> {this.props.text} </p>
+          <a href={this.props.link} target="_blank" class="btn btn-primary">
+            {" "}
+            Buy Now
+          </a>
+        </div>
+        <div>price: {this.props.price}</div>
+        <div>rating: {this.props.rating}</div>
+      </div>
+    );
+  }
+}
 
 export default Prop;
