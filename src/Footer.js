9354e30edd0afd1e2d1861ff48697d9aaf591b7e
Ramesh6698 <rameshsekar6698@gmail.com>
2023-05-21 22:00:27 +0530

    Task_Tracker

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+// import "./App.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="https://www.linkedin.com/in/abdullah-ahlatli">
+          <i class="fab fa-linkedin me-3"></i>
+        </a>
+        <a href="https://github.com/Sekunev">
+          <i class="fa-brands fa-github me-3"></i>
+        </a>
+      </div>
+      <article>© Copyright by Sekunev</article>
+    </footer>
+  );
+};
+
+export default Footer;
